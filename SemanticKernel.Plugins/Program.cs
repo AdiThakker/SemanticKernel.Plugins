49cab70e4007752171989d2f50ba5f165f3010e2
Adi Thakker <Aditya.Thakker@gmail.com>
2024-12-20 17:21:14 -0500

    initial working example using CreateFunctionFromPrompt

--- SemanticKernel.Plugins/Program.cs
+++ SemanticKernel.Plugins/Program.cs
@@ -1,32 +1,20 @@
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.ChatCompletion;
-using Microsoft.SemanticKernel.Plugins.Core;
 using Microsoft.SemanticKernel.TextGeneration;
+
 var builder = Kernel
                 .CreateBuilder()
                 .AddOllamaTextGeneration("phi3", new Uri("http://localhost:11434"));
 
 var kernel = builder.Build();
 
-var textGenerationService = kernel.GetRequiredService<ITextGenerationService>();
+var instructions = await File.ReadAllTextAsync("Plugin/instructions.txt");
+var csvData = await File.ReadAllTextAsync("Plugin/data.csv");
+var prompt = await File.ReadAllTextAsync("Plugin/prompt.txt");
 
-string promptTemplate = await File.ReadAllTextAsync("Plugins/prompts/prompts.txt");
-string instructions = await File.ReadAllTextAsync("Plugins/prompts/instructions.txt");
-string csvData = await File.ReadAllTextAsync("Plugins/prompts/data.csv");
-
-
-string formattedPrompt = promptTemplate
-    .Replace("{{$instructions}}", instructions)
-    .Replace("{{$csvData}}", csvData);
+var sqlPlugin = kernel.CreateFunctionFromPrompt(prompt);
 
 
-ChatHistory history = new()
-{
-    new Microsoft.SemanticKernel.ChatMessageContent(new AuthorRole("User"), formattedPrompt)
-};
-
-
-var response = await textGenerationService.GetTextContentAsync(formattedPrompt, kernel: kernel);
+var response = await sqlPlugin.InvokeAsync(kernel, new KernelArguments { ["instructions"] = instructions, ["csvData"] = csvData });
 
 Console.WriteLine("Generated SQL:");
 Console.WriteLine(response);
